44788adfe57c2276ef04dab598315bfde1862e9b
aku-abdi <74408765+aku-abdi@users.noreply.github.com>
2020-11-15 07:42:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'Odading BOT'; 
-const instagram = 'https://instagram.com/aditia_pribadiansyah/'; 
-const telegram = 'gak punya telegram :>'; 
+const BotName = 'YwNBoT'; 
+const instagram = 'https://www.instagram.com/yuanabdillah/'; 
+const telegram = 'https://t.me/ywnabdillah'; 
 const kapanbotaktif = '24 JAM'; 
-const youtube = 'gak punya cenel yutub :>';
+const youtube = 'punya tapi rahasia';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
@@ -64,7 +64,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@mwmaulana310')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@yuanabdillah')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
